0002f4a7cedec69de84a4402e5e7d7f1e859307a
Daisuke Hirakiuchi <daisuke.hirakiuchi@leonisand.co>
2015-05-12 16:39:05 +0900

    remove service command

--- lib/itamae/plugin/recipe/redis/package.rb
+++ lib/itamae/plugin/recipe/redis/package.rb
@@ -12,17 +12,9 @@ when 'debian', 'ubuntu'
   package 'redis-server' do
     version redis_version unless redis_version.nil?
   end
-
-  service 'redis-server' do
-    action [:enable, :start]
-  end
 when 'redhat', 'fedora'
   package 'redis' do
     version redis_version unless redis_version.nil?
     options '--enablerepo=epel'
-  end
-
-  service 'redis' do
-    action [:enable, :start]
   end
 end
