e31e9650ca5f5e0828510518736ff7c4e6931bd7
maruware <me@maruware.com>
2016-04-04 12:24:37 +0900

    Add support amazon linux

--- lib/itamae/plugin/recipe/redis/enable.rb
+++ lib/itamae/plugin/recipe/redis/enable.rb
@@ -4,7 +4,7 @@ service_name = \
   case node[:platform]
   when 'debian', 'ubuntu'
     'redis-server'
-  when 'redhat', 'fedora'
+  when 'redhat', 'fedora', 'amazon'
     'redis'
   else
     fail "Sorry, your platform(#{node[:platform]}) is not supported yet."
