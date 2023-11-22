5cbd00d1b3cdbe6149c5d0a1c26d98c162ae5930
gemmaro <gemmaro.dev@gmail.com>
2023-11-22 22:58:00 +0900

    Add node attribute for config path

--- lib/itamae/plugin/recipe/redis/config.rb
+++ lib/itamae/plugin/recipe/redis/config.rb
@@ -2,12 +2,13 @@
 
 node.reverse_merge!(
   redis: {
-    port: 6379
+    port: 6379,
+    config: '/etc/redis.conf'
   }
 )
 
 port = node[:redis][:port]
 execute 'configure redis port ' do
-  command %(sed -i -e 's/^port [0-9]\\{1,\\}$/port #{port}/g' /etc/redis.conf)
-  not_if %(grep -e '^port #{port}$' /etc/redis.conf)
+  command %(sed -i -e 's/^port [0-9]\\{1,\\}$/port #{port}/g' #{node[:redis][:config]})
+  not_if %(grep -e '^port #{port}$' #{node[:redis][:config]})
 end
